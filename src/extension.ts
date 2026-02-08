aa2a72ebec3bd1820d97c727faeffce63c35903c
dadul96 <46626392+dadul96@users.noreply.github.com>
2026-02-08 20:04:37 +0100

    Added empty autopilotProvider.ts and ollamaClient.ts files to help me structure the future architecture of the code; Registered the available commands in the extension.ts and added a short demo of the guiHandler and configHandler in action (still work in progress)

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,57 @@
 import * as vscode from 'vscode';
+import { ConfigHandler } from "./configHandler";
+import { GuiHandler } from "./guiHandler";
+//import { OllamaClient } from "./ollamaClient";
+//import { AutopilotProvider } from "./autopilotProvider";
+
 
 export function activate(context: vscode.ExtensionContext) {
-
+	const configHandler = new ConfigHandler();
+	const guiHandler = new GuiHandler(context, configHandler);
 	
-	const disposable = vscode.commands.registerCommand('ollama-autopilot.helloWorld', () => {
+	//const ollamaClient = new OllamaClient(configHandler);
+	//const autopilotProvider = new AutopilotProvider(ollamaClient, configHandler);
 
-	});
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand("ollama-autopilot.showMenu", () => {
+            guiHandler.showMenu();
+        }),
+    );
+	
+	context.subscriptions.push(
+        vscode.commands.registerCommand("ollama-autopilot.selectModel", () => {
+            
+        }),
+    );
+	
+	context.subscriptions.push(
+        vscode.commands.registerCommand("ollama-autopilot.enable", () => {
+			// for testing only - later use the configHandler for this:
+            const config = vscode.workspace.getConfiguration("ollama-autopilot");
+            config.update(
+                "general.autopilotEnabled",
+                true,
+                vscode.ConfigurationTarget.Global,
+            );
+
+			guiHandler.indicateOllamaEnabled();
+        }),
+    );
+	
+	context.subscriptions.push(
+        vscode.commands.registerCommand("ollama-autopilot.disable", () => {
+			// for testing only - later use the configHandler for this:
+			const config = vscode.workspace.getConfiguration("ollama-autopilot");
+            config.update(
+                "general.autopilotEnabled",
+                false,
+                vscode.ConfigurationTarget.Global,
+            );
+
+			guiHandler.indicateAutopilotDisabled();
+        }),
+    );
 }
 
 export function deactivate() {}
